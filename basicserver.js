0eefb61c33656ced94ee1be33938bfbdbb72a780
Marc Miles <marcmiles@gmail.com>
2011-07-13 10:45:07 -0700

    test server

--- basicserver.js
+++ basicserver.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(80, "ubuntu@ec2-50-18-142-132.us-west-1.compute.amazonaws.com");
+}).listen(80, "ec2-50-18-142-132.us-west-1.compute.amazonaws.com");
 console.log('Server running at http://ec2-50-18-142-132.us-west-1.compute.amazonaws.com');
